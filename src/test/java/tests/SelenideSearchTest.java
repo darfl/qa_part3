64fbc885d907d95a9d5081e9378a76c6c3e9beb1
Diana Darfl <diana.darfl@gmail.com>
2026-02-13 02:01:20 +0500

    added selenide lesson 1

---
+++ src/test/java/tests/SelenideSearchTest.java
@@ -0,0 +1,39 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideSearchTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void SelenideRepository() {
+        //открыть Selenide
+        open("/");
+        //ввести в поле поиска Selenide
+        //нажать enter
+        $("[placeholder='Search or jump to...']").click();
+        $("#query-builder-test").setValue("selenide").pressEnter();
+        //кликнуть на первый репозиторий из списка найденных
+        $("a[href*='selenide/selenide']").click();
+        //$(byAttribute("href", "/selenide/selenide")).click();
+        //проверка: selenide/selenide
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+    }
+}
+
+//.classname, #id1, []атрибут, a[href=abc].red
+//sleep(5000);
